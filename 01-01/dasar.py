4444cdb09798ec2311a1ce1e8096f59b2c1c8fc4
kharis12-web <hatami391998@gmail.com>
2020-09-02 14:33:59 +0700

    Intro Python

---
+++ 01-01/dasar.py
@@ -0,0 +1,93 @@
+print("my name is hatami")
+
+######################NUMBER###########################
+
+#pertambahan
+print(1+2)
+
+#pembagian
+print(30/2) # division always returns a floating point number
+
+#perkalihan
+print(80**2)
+
+#pengurangan
+print(10-3)
+
+#pangkat 
+print(10**10)
+
+#ukuran
+width = 20
+height = 4*5
+print(width*height)
+
+#floatingPoint
+print(4*3.75-1) 
+
+tax = 12.5/100
+price = 100.50
+print(price*tax)
+
+_=price*tax
+
+print(price +_)
+
+_ = price + _ 
+print(round(_, 2))
+#####################################STRING###################################
+
+print("spam egg")
+print("doesn't")
+print('"yes the said"')
+
+s = "ayon suka dia"
+print(s)
+
+#carachter Special
+print('C:\some\name')  # \n mean new line di bawah 
+print('C:\\some\\name')  # note the \\ between the quote
+print(r'C:\some\name')  # note the r before the quote
+
+p =('py' + 'thon')
+print(p)
+
+prefix = ('py')
+print(prefix+'thon')
+
+#list
+
+sue = [1,2,3,4,5]
+print(sue)
+print(sue[0]) #indexing returns the item
+print(sue[3])
+
+word = "Praxis Academy"
+print(word[0])
+
+
+apik = [1,2,2,4,5]
+print(apik)
+apik[2]=3
+print(apik)
+
+squares = [1,2,3,4,5]
+print(squares)
+
+print(squares[:])
+
+jumlah = squares + [1,3,4,5]
+print(jumlah)
+
+ayon = [1,2,3,4,5,6,7]
+
+for data  in ayon:
+    print(data+2)
+
+kelas = [1,2,3,4,5,6,7]
+
+for data in kelas :
+    print(data+1)
+cubes = [1, 8, 27, 65, 125]
+print(cubes[3])
+
