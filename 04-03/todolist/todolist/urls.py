9cd877705ad8455df91bd7ff82d279d746a14b0e
kharis12-web <hatami391998@gmail.com>
2020-09-15 09:37:47 +0700

    Refisi Pembuatan Aplikasi LogBook Mahasiswa Part 1

---
+++ 04-03/todolist/todolist/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('task/', include('task.urls')),
+    path('catatan/', include('catatan.urls')),
+    path('pegawai/', include('pegawai.urls')),
+    path('profil/', include('profil.urls')),
+    path('accounts/',include("accounts.urls")),
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
